54e5339508164bc7a5cc5a648583e7b9f401dc6a
unknown <bli@.(none)>
2010-02-09 22:17:04 -0600

    organize tests

--- src/test/scala/edu/ou/ece/dip/hir/shape/Point2DSpec.scala
+++ src/test/scala/edu/ou/ece/dip/hir/shape/Point2DSpec.scala
@@ -3,10 +3,14 @@ package edu.ou.ece.dip.hir.shape
 import edu.ou.ece.dip.hir.SpecificationBase
 
 class Point2DSpec extends SpecificationBase {
-  "Should get the distance to another point" in {
+  "Should get the distance to another point" >> {
     val from = new Point2D(2, 2)
-    val to = new Point2D(0, 2)
+    "Given a point at the same position" >> {
+      from.distanceTo(from) must_== 0
+    }
 
-    from.distanceTo(to) must_== 2
+    "Given a point not at the same position" >> {
+      from.distanceTo(Point2D(0, 2)) must_== 2
+    }
   }
 }
--- src/test/scala/edu/ou/ece/dip/hir/shape/VectorSpec.scala
+++ src/test/scala/edu/ou/ece/dip/hir/shape/VectorSpec.scala
@@ -1,34 +1,32 @@
 package edu.ou.ece.dip.hir.shape
 
-import org.specs.runner.ScalaTest
 import org.specs.Specification
 
-class VectorSpec extends Specification with ScalaTest {
-  "Dot product" should {
-    "returns zero when the angle is 90" in {
-      val vectorA = new Vector(new Line3D(new Point3D(0, 0, 0), new Point3D(1, 0, 0)))
-      val vectorB = new Vector(new Line3D(new Point3D(0, 0, 0), new Point3D(0, 1, 0)))
+class VectorSpec extends Specification {
+  "A vector" should {
+    "do dot product" >> {
 
-      vectorA.dotProduct(vectorB) must_== 0
+      "Given a vector whose is angle is 90" +
+              "Then the dot product should be zero" >> {
+        val vectorA = new Vector(new Line3D(new Point3D(0, 0, 0), new Point3D(1, 0, 0)))
+        val vectorB = new Vector(new Line3D(new Point3D(0, 0, 0), new Point3D(0, 1, 0)))
+        vectorA.dotProduct(vectorB) must_== 0
+      }
 
+      "Given" >> {
+        val vectorA = new Vector(new Line3D(new Point3D(1, 1, 0), new Point3D(3, 3, 0)))
+        val vectorB = new Vector(new Line3D(new Point3D(1, 1, 0), new Point3D(5, 5, 0)))
+        vectorA.dotProduct(vectorB) must_== 16
+      }
     }
 
-    "should do dot product" in {
-
-      val vectorA = new Vector(new Line3D(new Point3D(1, 1, 0), new Point3D(3, 3, 0)))
-      val vectorB = new Vector(new Line3D(new Point3D(1, 1, 0), new Point3D(5, 5, 0)))
-
-      vectorA.dotProduct(vectorB) must_== 16
+    "do corss product" >> {
+      
+      "Given" >> {
+        val vectorA = new Vector(1, 2, 3)
+        val vectorB = new Vector(4, 5, 6)
+        vectorA.crossProduct(vectorB) must_== new Vector(-3, 6, -3)
+      }
     }
-
   }
-
-  "should do cross product" in {
-
-    val vectorA = new Vector(1, 2, 3)
-    val vectorB = new Vector(4, 5, 6)
-
-    vectorA.crossProduct(vectorB) must_== new Vector(-3, 6, -3)
-  }
-
 }
