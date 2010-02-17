e8a77af28a0e8af3b480d5701e76c9d3fc2a5e4e
unknown <bli@.(none)>
2010-02-16 21:48:21 -0600

    parse lines from for matlab interop

--- src/main/scala/edu/ou/ece/dip/hlr/utils/MatlabInterop.scala
+++ src/main/scala/edu/ou/ece/dip/hlr/utils/MatlabInterop.scala
@@ -3,7 +3,8 @@ package edu.ou.ece.dip.hlr.utils
 import collection.mutable.ListBuffer
 import edu.ou.ece.dip.hlr.shape._
 
-class PointDto(val x: Double, val y: Double, val z: Double, val index: Int, val shapeId: Int)
+case class PointDto(x: Double, y: Double, z: Double, index: Int, shapeId: Int)
+case class LineDto(s_x: Double, s_y: Double, s_z: Double, e_x: Double, e_y: Double, e_z: Double, index: Int)
 
 object MatlabInterop {
   def generateMatrixForLineDisplay(lines: List[Line2D]): String = {
@@ -16,12 +17,8 @@ object MatlabInterop {
     "figure;" + builder.toString
   }
 
-  def parseLines(): List[Line3D] = {
-    List()    
-  }
-
   def parseShapes(x: Array[Double], y: Array[Double], z: Array[Double],
-            index: Array[Int], id: Array[Int]): List[Decomposable] = {
+                  index: Array[Int], id: Array[Int]): List[Decomposable] = {
     val points = new ListBuffer[PointDto];
     for (i <- 0 until x.length) {
       points += new PointDto(x(i), y(i), z(i), index(i), id(i))
@@ -35,8 +32,22 @@ object MatlabInterop {
       shapes += res._1
       pointsToProcess = res._2
     }
-    
+
     shapes.toList
+  }
+
+  def parseLines(line_start_x: Array[Double], line_start_y: Array[Double], line_start_z: Array[Double],
+                 line_end_x: Array[Double], line_end_y: Array[Double], line_end_z: Array[Double],
+                 line_index: Array[Int]): List[LineDto] = {
+
+    //can't use for...yield since a scala bug http://www.eishay.com/2009/06/unexpected-repeated-execution-in-scala.html
+    val res = new ListBuffer[LineDto]
+
+    val value = for (i <- 0 until line_start_x.length) {
+      res.append(LineDto(line_start_x(i), line_start_y(i), line_start_z(i), line_end_x(i), line_end_y(i), line_end_z(i), line_index(i)))
+    }
+
+    res.toList
   }
 
   def removeHiddenLine(shape_x: Array[Double], shape_y: Array[Double], shape_z: Array[Double],
@@ -47,10 +58,10 @@ object MatlabInterop {
                        out_start_x: Array[Double], out_start_y: Array[Double],
                        out_end_x: Array[Double], out_end_y: Array[Double],
                        original_index: Array[Int]) {
-//    val decomopsables = parseShapes(shape_x, shape_y, shape_z, index, shapeId)
-//    val triangles = for(d <- decomopsables) yield d.triangles
-//    val lines = parseLines(shape_x, shape_y, shape_z, index, shapeId)
-//    HiddenLineRemovalUtils.removeHiddenLines(triangles, lines, focalLength)
+    //    val decomopsables = parseShapes(shape_x, shape_y, shape_z, index, shapeId)
+    //    val triangles = for(d <- decomopsables) yield d.triangles
+    //    val lines = parseLines(shape_x, shape_y, shape_z, index, shapeId)
+    //    HiddenLineRemovalUtils.removeHiddenLines(triangles, lines, focalLength)
   }
 
   private def toPoint(p: PointDto): Point3D = {
--- src/test/scala/edu/ou/ece/dip/hlr/utils/MatlabInteropSpec.scala
+++ src/test/scala/edu/ou/ece/dip/hlr/utils/MatlabInteropSpec.scala
@@ -4,6 +4,7 @@ import edu.ou.ece.dip.hlr.SpecificationBase
 import edu.ou.ece.dip.hlr.shape.{Trapezoid3D, Point3D, Triangle3D}
 
 class MatlabInteropSpec extends SpecificationBase {
+  
   "remove hidden line" should {
     val a_x = 0.0
     val a_y = 0.0
@@ -27,49 +28,89 @@ class MatlabInteropSpec extends SpecificationBase {
     val C = Point3D(c_x, c_y, c_z)
     val D = Point3D(d_x, d_y, d_z)
 
-    "extract one triangle from the input containing one triangle" >> {
+    "parse shapes" >> {
+
+      "extract one triangle from the input containing one triangle" >> {
 
 
-      val index = Array(0, 1, 2)
-      val shapeId = Array(3, 3, 3)
+        val index = Array(0, 1, 2)
+        val shapeId = Array(3, 3, 3)
 
-      val x_list = Array(a_x, b_x, c_x)
-      val y_list = Array(a_y, b_y, c_y)
-      val z_list = Array(a_z, b_z, c_z)
+        val x_list = Array(a_x, b_x, c_x)
+        val y_list = Array(a_y, b_y, c_y)
+        val z_list = Array(a_z, b_z, c_z)
 
-      val actual = MatlabInterop.parseShapes(x_list, y_list, z_list, index, shapeId)
+        val actual = MatlabInterop.parseShapes(x_list, y_list, z_list, index, shapeId)
 
-      actual.length must_== 1
+        actual.length must_== 1
 
-      val t = actual(0).asInstanceOf[Triangle3D]
+        val t = actual(0).asInstanceOf[Triangle3D]
 
-      t.getVertexAB must_== A.to(B)
-      t.getVertexAB.name must_== "0"
-      t.getVertexBC must_== B.to(C)
-      t.getVertexBC.name must_== "1"
-      t.getVertexCA must_== C.to(A)
-      t.getVertexCA.name must_== "2"
+        t.getVertexAB must_== A.to(B)
+        t.getVertexAB.name must_== "0"
+        t.getVertexBC must_== B.to(C)
+        t.getVertexBC.name must_== "1"
+        t.getVertexCA must_== C.to(A)
+        t.getVertexCA.name must_== "2"
+      }
+
+      "extract one trapezoid from the input containing one trapezoid" >> {
+        val index = Array(0, 1, 2, 3)
+        val shapeId = Array(4, 4, 4, 4)
+
+        val x_list = Array(a_x, b_x, c_x, d_x)
+        val y_list = Array(a_y, b_y, c_y, d_y)
+        val z_list = Array(a_z, b_z, c_z, d_z)
+
+        val actual = MatlabInterop.parseShapes(x_list, y_list, z_list, index, shapeId)
+
+        actual.length must_== 1
+
+        val t = actual(0).asInstanceOf[Trapezoid3D]
+
+        t.cornerA must_== A
+        t.cornerB must_== B
+        t.cornerC must_== C
+        t.cornerD must_== D
+      }
     }
 
-    "extract one trapezoid from the input containing one trapezoid" >> {
-      val index = Array(0, 1, 2, 3)
-      val shapeId = Array(4, 4, 4, 4)
+    "parse lines" >> {
 
-      val x_list = Array(a_x, b_x, c_x, d_x)
-      val y_list = Array(a_y, b_y, c_y, d_y)
-      val z_list = Array(a_z, b_z, c_z, d_z)
+      val line_start_x = Array(0.0, 1.0)
+      val line_start_y = Array(0.0, 1.0)
+      val line_start_z = Array(0.0, 1.0)
 
-      val actual = MatlabInterop.parseShapes(x_list, y_list, z_list, index, shapeId)
+      val line_end_x = Array(5.0, 6.0)
+      val line_end_y = Array(5.0, 6.0)
+      val line_end_z = Array(5.0, 6.0)
 
-      actual.length must_== 1
+      val index = Array(0, 1)
 
-      val t = actual(0).asInstanceOf[Trapezoid3D]
+      val lines = MatlabInterop.parseLines(line_start_x, line_start_y, line_start_z, line_end_x, line_end_y, line_end_z, index)
 
-      t.cornerA must_== A
-      t.cornerB must_== B
-      t.cornerC must_== C
-      t.cornerD must_== D
+      lines.length must_== 2
+      
+      lines(0).s_x must_== line_start_x(0)
+      lines(0).s_y must_== line_start_y(0)
+      lines(0).s_z must_== line_start_z(0)
+      lines(0).e_x must_== line_end_x(0)
+      lines(0).e_y must_== line_end_y(0)
+      lines(0).e_z must_== line_end_z(0)
+      lines(0).index must_== index(0)
+
+      lines(1).s_x must_== line_start_x(1)
+      lines(1).s_y must_== line_start_y(1)
+      lines(1).s_z must_== line_start_z(1)
+      lines(1).e_x must_== line_end_x(1)
+      lines(1).e_y must_== line_end_y(1)
+      lines(1).e_z must_== line_end_z(1)
+      lines(1).index must_== index(1)      
+
+
+
     }
+
 
   }
 }
